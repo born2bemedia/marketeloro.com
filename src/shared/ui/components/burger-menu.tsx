b6d47039d857a2bfcb5660ed7fca9a035589a9c4
daniil-b2be <dan.developer@born2be.media>
2025-09-17 17:33:24 +0300

    burger menu

---
+++ src/shared/ui/components/burger-menu.tsx
@@ -0,0 +1,118 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useTranslations } from 'next-intl';
+import {
+  Close,
+  Content,
+  Description,
+  Portal,
+  Root,
+  Title,
+  Trigger,
+} from '@radix-ui/react-dialog';
+
+import { PhoneIcon } from '../icons/phone';
+import { SmsIcon } from '../icons/sms';
+import { Text } from '../kit/text';
+import { SocialNetworks } from './social-networks';
+import { Link as NavLink } from '@/i18n/navigation';
+
+export const BurgerMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  const pathname = usePathname();
+
+  useEffect(() => setOpen(false), [pathname]);
+
+  useEffect(() => {
+    document.documentElement.style.overflow = open ? 'hidden' : '';
+
+    return () => {
+      document.documentElement.style.overflow = '';
+    };
+  }, [open]);
+
+  const t = useTranslations('header');
+
+  return (
+    <Root open={open} onOpenChange={setOpen}>
+      <Trigger>
+        <section className="hidden max-md:flex">
+          <Text>{t('menu', { fallback: 'Menu' })}</Text>
+        </section>
+      </Trigger>
+      <Portal>
+        <Content className="fixed bottom-0 left-0 z-[995] h-screen w-screen animate-[menuOpen_0.3s_ease-out_forwards] overflow-y-auto bg-white">
+          <Title />
+          <Description asChild>
+            <section className="flex h-full flex-col justify-between gap-5 px-2 pt-2 pb-4">
+              <section className="flex flex-col gap-5">
+                <section className="flex items-center justify-between">
+                  <Image src="/logo.svg" alt="Logo" width={37} height={40} />
+                  <div className="flex items-center">
+                    <Close>
+                      <Text>{t('close', { fallback: 'Close' })}</Text>
+                    </Close>
+                  </div>
+                </section>
+                <section className="flex flex-col gap-5">
+                  <NavLink href="/services" className="pl-2.5">
+                    <Text size="xl">
+                      {t('services', { fallback: 'Services' })}
+                    </Text>
+                  </NavLink>
+                  <NavLink href="/tailored-plans" className="pl-2.5">
+                    <Text size="xl">{t('plans', { fallback: 'Plans' })}</Text>
+                  </NavLink>
+                  <NavLink href="/about-us" className="pl-2.5">
+                    <Text size="xl">
+                      {t('aboutUs', { fallback: 'About Us' })}
+                    </Text>
+                  </NavLink>
+                  <NavLink href="/story-time" className="pl-2.5">
+                    <Text size="xl">
+                      {t('storyTime', { fallback: 'Story Time' })}
+                    </Text>
+                  </NavLink>
+                  <NavLink
+                    href="/contact-us"
+                    className="rounded-3xl bg-[#030213] px-2.5 py-1"
+                  >
+                    <Text size="xl" color="white">
+                      {t('contactUs', { fallback: 'Contact Us' })}
+                    </Text>
+                  </NavLink>
+                </section>
+              </section>
+              <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-4 px-2.5">
+                  <Link
+                    href="tel:+15551234567"
+                    className="flex items-center gap-1"
+                  >
+                    <PhoneIcon />
+                    +1 (555) 123-4567
+                  </Link>
+                  <Link
+                    href="mailto:info@marketeloro.com"
+                    className="flex items-center gap-1"
+                  >
+                    <SmsIcon />
+                    info@marketeloro.com
+                  </Link>
+                </div>
+                <div className="flex flex-col gap-5">
+                  <SocialNetworks />
+                </div>
+              </div>
+            </section>
+          </Description>
+        </Content>
+      </Portal>
+    </Root>
+  );
+};
