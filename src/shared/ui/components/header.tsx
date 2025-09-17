0670c2b96446937dcec5290df943b89fc14aa0e1
daniil-b2be <dan.developer@born2be.media>
2025-09-17 17:50:48 +0300

    lang switcher

--- src/shared/ui/components/header.tsx
+++ src/shared/ui/components/header.tsx
@@ -4,14 +4,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
-import { CartIcon } from '@/shared/ui/icons/cart';
 import { MailIcon } from '@/shared/ui/icons/mail';
 import { PhoneIcon } from '@/shared/ui/icons/phone';
-import { Button } from '@/shared/ui/kit/button';
 import { Text } from '@/shared/ui/kit/text';
 
+import { Button } from '../kit/button';
 import { BurgerMenu } from './burger-menu';
+import { LangSwitcher } from './lang-switcher';
 import { SocialNetworks } from './social-networks';
+import { Link as NavLink } from '@/i18n/navigation';
 
 export const Header = () => {
   const t = useTranslations('header');
@@ -20,15 +21,15 @@ export const Header = () => {
     <header className="flex items-center justify-between px-10 pt-2 pb-3.5 max-md:px-2">
       <Image src="/logo.svg" alt="Logo" width={37} height={40} />
       <nav className="flex items-center gap-7 max-md:hidden">
-        <Link href="/services">
+        <NavLink href="/services">
           <Text>{t('services', { fallback: 'Services' })}</Text>
-        </Link>
-        <Link href="/about-us">
+        </NavLink>
+        <NavLink href="/about-us">
           <Text>{t('aboutUs', { fallback: 'About Us' })}</Text>
-        </Link>
-        <Link href="/about-us">
+        </NavLink>
+        <NavLink href="/about-us">
           <Text>{t('contact', { fallback: 'Contact' })}</Text>
-        </Link>
+        </NavLink>
       </nav>
       <section className="flex items-center gap-6 max-md:hidden">
         <div className="flex items-center gap-4">
@@ -45,6 +46,14 @@ export const Header = () => {
           </Link>
         </div>
         <SocialNetworks />
+        <div className="flex items-center gap-6">
+          <NavLink href="/contact-us">
+            <Button className="py-1">
+              {t('contactUs', { fallback: 'Contact Us' })}
+            </Button>
+          </NavLink>
+          <LangSwitcher />
+        </div>
       </section>
       <section className="hidden max-md:flex">
         <BurgerMenu />
